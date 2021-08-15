79506b7a6250c2a8cd2d040c9d2c089fc3e4c1bd
Ksenia Shurina <kseniashurina18@gmail.com>
2021-08-15 14:26:25 +0300

    HW7

---
+++ HW7/HW7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW7
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write($"Введите количесвто элементов массива ");
+            int elementsCount = int.Parse(Console.ReadLine());
+            int[] thisElements = new int[elementsCount];
+            for(int i = 0; i < thisElements.Length; i++)
+            {
+                Console.Write($"Введите {i} элемент массива: ");
+                thisElements[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Элементы массива: ");
+            for (int i = 0; i < thisElements.Length; i++)
+            {
+                Console.Write(thisElements[i] + " ");
+            }
+            Console.Write("\nПеревёрнутый массив: ");
+            for (int i = thisElements.Length -1; i>= 0; i--)
+            {
+                Console.Write(thisElements[i] + " ");
+            }
+        }
+    }
+}
---
+++ HW7/HW7_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums = new int[3, 3]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+            int rows = nums.GetUpperBound(0) + 1;
+            int columns = nums.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                int rowMaxValue = nums[i, 0];
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{nums[i, j]} \t");
+                    if (nums[i, j] > rowMaxValue)
+                    {
+                        rowMaxValue = nums[i, j];
+                    }
+                }
+                Console.Write($"\t Максимальное значение строки: {rowMaxValue}");
+                Console.WriteLine();
+            }
+            //Console.Write("Введите элементы 1 массива ");
+            //int otherElementsCount = int.Parse(Console.ReadLine());
+            //Console.Write("Введите элементы 2 массива ");
+            //int otherElementsCount2 = int.Parse(Console.ReadLine());
+            //int[,] nums = new int[otherElementsCount, otherElementsCount2];
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    for (int a = 0; a < 2; a++)
+            //    {
+            //        nums[i, a] = otherElementsCount;
+            //        Console.Write("{0}\t", nums[i, a]);
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
