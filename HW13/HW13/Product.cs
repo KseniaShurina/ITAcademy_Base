8a46cd6c51cd812de551aef0260690952807836b
Ksenia Shurina <kseniashurina18@gmail.com>
2021-09-11 19:21:10 +0300

    HW13

---
+++ HW13/HW13/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW13
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string NameStore { get; set; }
+        
+        public Product(string name, string nameStore)
+        {
+            Name = name;
+            NameStore = nameStore;
+        }
+        public override string ToString()
+        {
+            return $"{Name}:{NameStore}";
+        }
+    }
+}
---
+++ HW13/HW13/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[5];
+            products[0] = new Product("milk", "Almi");
+            products[1] = new Product("coffee", "Almi");
+            products[2] = new Product("sugar", "Prostore");
+            products[3] = new Product("vodca", "Evroopt");
+            products[4] = new Product("juce", "Green");
+
+            for (int i = 0; i < products.Length; i++)
+                Console.WriteLine("{0}. {1}", i + 1, products[i]);
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                for (int j = 0; j < products.Length - 1; j++) 
+                { 
+                
+                    if (string.Compare(products[j].NameStore, products[j + 1].NameStore) > 0)
+                    {
+                        var s = products[i];
+                        products[i] = products[j + 1];
+                        products[j + 1] = s;
+                    }
+                }
+            }
+
+            Console.WriteLine("Введите название магазина: ");
+            string nameStore = Console.ReadLine().ToUpper();
+
+            List<Product> foundProducts = new List<Product>();
+            foreach (Product i in products)
+            {
+                if (nameStore == i.NameStore.ToUpper())
+                {
+                    
+                    foundProducts.Add(i);
+                }
+            }
+
+            if(foundProducts.Count == 0)
+            {
+                Console.WriteLine($"Магазин {nameStore} не найден");
+            }
+
+            foreach(Product a in foundProducts)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
