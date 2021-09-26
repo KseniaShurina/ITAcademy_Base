c1fd59aa12cfbfc647b98abe3267ed47dc2deec7
Ksenia Shurina <kseniashurina18@gmail.com>
2021-09-26 21:59:11 +0300

    HW19_Asynchronous

---
+++ HW19/HW19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HW19
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Consistently
+            await Print("asd", 2);
+            await Print("fff", 2);
+
+            // Parallel
+            await Task.WhenAll(Print("asd2", 2), Print("fff2", 2));
+        }
+
+        public static async Task Print(string str, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(str);
+                await Task.Delay(100);
+            }
+        }
+    }
+}
